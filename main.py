77fb935368810d3eb3179e8ecb18984d567fd73a
Jainam Shroff <jainam.shroff@outlook.com>
2020-08-11 14:01:29 +0530

    Added Initial Files
    Uploaded Initial Files

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+
+loop = 0
+initial = time.time()
+print(initial)
+# this gives us ticks which increase one per second
+
+while(loop < 45):
+    print("This Is Jainam Shroff")
+    loop = loop + 1
+
+# We subtracted current ticks with initial ticks thus given ticks used by while loop
+print("While Loop Took: ", time.time() - initial)
+initial1 = time.time()
+
+for i in range(45):
+    print("This Is Jainam Shroff")
+
+# We subtracted current tick by the tick that was before for loop ran thus giving
+# Time taken by the for loop.
+print("For Loop Took: ", time.time() - initial1)
